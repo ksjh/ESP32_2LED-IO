5c86989c5924dbdfdf462f5dc0508067effbb4f1
ksjh <30961140+ksjh@users.noreply.github.com>
2025-09-14 12:43:13 +0200

    Arduino code cleanup

--- PlatformIO/ESP32_2LED-IO/include/serial-led-io.h
+++ PlatformIO/ESP32_2LED-IO/include/serial-led-io.h
@@ -9,4 +9,29 @@
 #define LEDON    LOW
 #define LEDOFF   HIGH
 
+void setleds(uint8_t status);
+uint8_t getleds();
+
+void setleds(uint8_t status)
+{
+    if (status & 0x01)
+        digitalWrite(LED1OUT, LEDON);
+    else
+        digitalWrite(LED1OUT, LEDOFF);
+    if (status & 0x02)
+        digitalWrite(LED2OUT, LEDON);
+    else
+        digitalWrite(LED2OUT, LEDOFF);
+}
+
+uint8_t getleds()
+{
+    uint8_t status = 0;
+    if (digitalRead(LED1STAT) == HIGH)
+        status |= 0x1;
+    if (digitalRead(LED2STAT) == HIGH)
+        status |= 0x2;
+    return status;
+}
+
 #endif // SERIAL_LED_IO_H_
--- PlatformIO/ESP32_2LED-IO/src/serial-led-io.cpp
+++ PlatformIO/ESP32_2LED-IO/src/serial-led-io.cpp
@@ -1,36 +1,14 @@
 #include <arduino.h>
 #include "serial-led-io.h"
 
-char schi;
+int schi;
 uint8_t sled, rled;
-
-void setleds(uint8_t status)
-{
-    if (status & 0x01)
-        digitalWrite(LED1OUT, LEDON);
-    else
-        digitalWrite(LED1OUT, LEDOFF);
-    if (status & 0x02)
-        digitalWrite(LED2OUT, LEDON);
-    else
-        digitalWrite(LED2OUT, LEDOFF);
-}
-
-uint8_t getleds()
-{
-    uint8_t status = 0;
-    if (digitalRead(LED1STAT) == HIGH)
-        status |= 0x1;
-    if (digitalRead(LED2STAT) == HIGH)
-        status |= 0x2;
-    return status;
-}
 
 void setup()
 {
     pinMode(LED1OUT, OUTPUT);
-    pinMode(LED2OUT, OUTPUT);
     digitalWrite(LED1OUT, LEDOFF);
+    pinMode(LED2OUT, OUTPUT);
     digitalWrite(LED2OUT, LEDOFF);
     pinMode(LED1STAT, INPUT);
     pinMode(LED2STAT, INPUT);
@@ -38,15 +16,16 @@ void setup()
 
 void loop()
 {
-    while (Serial.available())
+    while (Serial && Serial.available())
     {
-        schi = (char)Serial.read();
+        schi = Serial.read();
         if (schi >= '0' && schi < '4')
         {
-            sled = schi - '0';
+            sled = (uint8_t)(schi - '0');
             setleds(sled);
         }
         rled = getleds();
-        Serial.write('0' + rled);
+        if (Serial && Serial.availableForWrite())
+            Serial.write(char('0' + rled));
     }
 }
